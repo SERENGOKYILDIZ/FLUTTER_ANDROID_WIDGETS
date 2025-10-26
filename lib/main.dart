496b6e6003c11953d6eb9d142e285a8ba889209b
Semi Eren Gokyildiz <127510869+SERENGOKYILDIZ@users.noreply.github.com>
2025-10-26 14:12:28 +0300

    Learned using of TextField, Button and Text

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
@@ -27,17 +28,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  var tfController = TextEditingController();
+  String alinanVeri = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.deepOrangeAccent,
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            TextField(
+              controller: tfController,
+              decoration: InputDecoration(
+                hintText: "Yazınız",
+              ),
+            ),
+            ElevatedButton(
+                onPressed: (){
+                  setState(() {
+                    alinanVeri = tfController.text; /// Kontrolciden veriyi alıp günceller.
+                  });
+                },
+                child: Text("Veriyi Al")
+            ),
+            Text("Gelen Veri : ${alinanVeri}"),
           ],
         ),
       ),
